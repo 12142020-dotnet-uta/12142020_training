40eac66f0ca7407d9d8d8d70fa256c51004eeea2
Mark Moore <mark.moore@revature.com>
2020-12-14 18:09:32 -0600

    adds day one demo and days 1 and 2 pdf's

--- Demos/Week1/HelloWorldDemo/Program.cs
+++ Demos/Week1/HelloWorldDemo/Program.cs
@@ -7,6 +7,16 @@ namespace HelloWorldDemo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter a word.");
+            string response = Console.ReadLine();
+            Console.WriteLine($"Your response is {response}.");
+
+            Console.WriteLine("Hey ");
+            Console.WriteLine("there ");
+            Console.WriteLine("tiger. ");
+            Console.WriteLine("Ahh! ");
+
+
         }
     }
 }

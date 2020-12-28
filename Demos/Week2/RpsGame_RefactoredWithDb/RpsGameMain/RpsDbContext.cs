f9483513c8c5be5920cd7a12db6a37addd63a461
Mark Moore <mark.moore@revature.com>
2020-12-28 15:58:24 -0600

    adds some pdfs and demos (accidentally on this branch)

--- Demos/Week2/RpsGame_RefactoredWithDb/RpsGameMain/RpsDbContext.cs
+++ Demos/Week2/RpsGame_RefactoredWithDb/RpsGameMain/RpsDbContext.cs
@@ -9,9 +9,15 @@ namespace RpsGame_NoDb
         public DbSet<Round> rounds { get; set; }
         public DbSet<Match> matches { get; set; }
 
+        public RpsDbContext() { }
+        public RpsDbContext(DbContextOptions options) : base(options) { }
+
         protected override void OnConfiguring(DbContextOptionsBuilder options)
         {
-            options.UseSqlServer("Server=localhost\\SQLEXPRESS02;Database=RpsGame12142020;Trusted_Connection=True;");
+            if (!options.IsConfigured)
+            {
+                options.UseSqlServer("Server=localhost\\SQLEXPRESS02;Database=RpsGame12142020;Trusted_Connection=True;");
+            }
         }
 
 

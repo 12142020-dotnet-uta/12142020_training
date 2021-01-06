277fa5c0186cf4ff978aecaae1d132340f72df47
Mark Moore <mark.moore@revature.com>
2021-01-06 13:41:37 -0600

    adds Day2 of week3/MvcRpsDemo

---
+++ Demos/Week3/12142020_MvcRpsDemo/BusinessLogicLayer/MapperClass.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using ModelLayer;
+using ModelLayer.ViewModels;
+
+namespace BusinessLogicLayer
+{
+	public class MapperClass
+	{
+
+		public PlayerViewModel ConvertPlayerToPlayerViewModel(Player player)
+		{
+			PlayerViewModel playerViewModel = new PlayerViewModel()
+			{
+				playerId = player.playerId,
+				Fname = player.Fname,
+				Lname = player.Lname,
+				numLosses = player.numLosses,
+				numWins = player.numWins,
+				JpgStringImage = ConvertByteArrayToJpgString(player.ByteArrayImage)
+			};
+
+			return playerViewModel;
+		}
+
+		private string ConvertByteArrayToJpgString(byte[] byteArray)
+		{
+			if (byteArray != null)
+			{
+				string imageBase64Data = Convert.ToBase64String(byteArray, 0, byteArray.Length);
+				string imageDataURL = string.Format($"data:image/jpg;base64,{imageBase64Data}");
+				return imageDataURL;
+			}
+			else return null;
+		}
+
+		public byte[] ConvertIformFileToByteArray(IFormFile iformFile)
+		{
+			using (var ms = new MemoryStream())
+			{
+				// convert the IFormFile into a byte[]
+				iformFile.CopyTo(ms);
+
+				if (ms.Length > 2097152)// if it's bigger that 2 MB
+				{
+					return null;
+				}
+				else
+				{
+					byte[] a = ms.ToArray(); // put the string into the Image property
+					return a;
+				}
+			}
+		}
+	}
+}
